706015f90df3827fb19d49b1079d4e86e3fdd85f
KaiAtDCI <kaiatdci@web.de>
2021-03-29 13:34:37 +0200

    first commit

---
+++ console-log-exercise.js
@@ -0,0 +1,14 @@
+const firstName = "Kai";
+const lastName = "Grube";
+const street = "Bahnhofstraße";
+const houseNumber = "1";
+const postalCode = "22099";
+const city = "Hamburg";
+
+console.log ("Firstname: " + firstName);
+console.log ("Last name: " + lastName);
+console.log ("Street, house: " + street + ", " + houseNumber);
+console.log ("Postal code, city: " + postalCode + ", " + city);
+
+
+
